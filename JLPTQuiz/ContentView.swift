161b3c8aa8cd9aca5457d53f520472503aa41f10
ThidarPhyo <thidarphyo22031996@gmail.com>
2022-09-18 01:11:10 +0900

    Initial Commit

---
+++ JLPTQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JLPTQuiz
+//
+//  Created by Thidar Phyo on 9/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JLPTQuiz/JLPTQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  JLPTQuizApp.swift
+//  JLPTQuiz
+//
+//  Created by Thidar Phyo on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct JLPTQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
